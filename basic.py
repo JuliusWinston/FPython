42a65911288f33bd7680fcfcb5550603254a3b70
dev_winston <juliuswinston7@gmail.com>
2023-04-29 13:05:18 +0000

    First lesson

---
+++ basic.py
@@ -0,0 +1,27 @@
+# A program that asks the user for his/her password and gives or denies access based on the password
+
+password = "opensesame"
+print("Enter password to continue: ") # # Do something
+typePass = input()
+if typePass == password:
+    print("Access Granted, You may go ahead now!")
+else:
+    print("You do not have access to this section, please leave!")
+
+# for i in range(20): #Do this action exactly 20 times
+#     print("Hello World!")
+
+# i = 0
+# while i < 20:
+#     print("Hello world!")
+#     i=i+1
+
+# Asking Smart Programming Questions
+
+# 1. Explain what you are trying to do, not just what you did.
+# 2. Specify line at which point the error occurs.
+# 3. Copy and paste the entire error message and your code to the platform links you want to post.
+# 4. Explain what you've already tried to do to fix the program.
+# 5. List the version of python you're using.
+# 6. If error occurs after a particular change, add it.
+# 7. Say whether you're able to reproduce the error every time you run the program.
